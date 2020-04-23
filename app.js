af61da2d4494143301ab8f9f2d42a5da029e4363
DevDayz <kjyanus@gmail.com>
2020-04-23 17:51:59 +0900

    CanvasRenderingContext

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
+//config
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+ctx.strokeStyle = "#1e2022";
+ctx.lineWidth = 2.5;
 let painting = false;
+
+//Functions
+function stopPaint() {
+  painting = false;
+}
+function startPaint() {
+  painting = true;
+}
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    // ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 function onMouseDown() {
-  painting = true;
+  startPaint();
+  //somethinghere
 }
-function onMouseUp() {
-  painting = false;
-}
-function stopPaint() {
-  painting = false;
-}
+
+//Initiate
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseup", stopPaint);
   canvas.addEventListener("mouseleave", stopPaint);
 }
