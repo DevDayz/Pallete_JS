66a6c76b43e0386e576f5a11d7ec37aac31845e9
DevDayz <kjyanus@gmail.com>
2020-04-23 20:44:51 +0900

    Change brush color

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-//config
+//Declaration
 const canvas = document.querySelector("#jsCanvas");
+const colors = document.querySelectorAll(".controls__colorSet");
+let painting = false;
 const ctx = canvas.getContext("2d");
+
+//Configuration
 ctx.strokeStyle = "#1e2022";
 ctx.lineWidth = 2.5;
-let painting = false;
 
 //Functions
 function stopPaint() {
@@ -17,22 +20,29 @@ function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if (!painting) {
-    // ctx.beginPath();
+    ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
     ctx.stroke();
   }
 }
-function onMouseDown() {
-  startPaint();
-  //somethinghere
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 //Initiate
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mousedown", startPaint);
   canvas.addEventListener("mouseup", stopPaint);
   canvas.addEventListener("mouseleave", stopPaint);
 }
+
+Array.from(colors).forEach(colorSet => {
+  const colorset = Array.from(colorSet.children);
+  colorset.forEach(color => {
+    color.addEventListener("click", handleColorClick);
+  });
+});
